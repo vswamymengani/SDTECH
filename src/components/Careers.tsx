fabe7dec1e6d04217f6dc50fc3facdde668bcdbd
divyaNarasimhuludivya <140786101+divyaNarasimhuludivya@users.noreply.github.com>
2024-06-26 05:56:01 +0530

    old react js converted into new react vite and navigation also added.

---
+++ src/components/Careers.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Careers = () => {
+  return (
+    <div id="b1">
+      <h1>Our Careers:</h1>
+      <p id="b2"> It’s the most exciting time to be joining Sevak Digital Technologies,
+         and more specifically, SDK in India. We have countless
+          opportunities for career growth and development, the excitement
+           and pace of a startup, and the backing of a world-renowned global organization.</p>
+    </div>
+  );
+};
+
+export default Careers;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import Logo from '../assets/Logo-sevak.jpg';
+import { useNavigate } from 'react-router-dom';
+
+interface DropdownState {
+  [key: string]: boolean;
+}
+
+const Navbar: React.FC = () => {
+  const [dropdown, setDropdown] = useState<DropdownState>({ products: false, services: false, contact: false });
+  const navigate = useNavigate();
+
+  const handleMouseEnter = (menu: string) => {
+    setDropdown(prevState => ({ ...prevState, [menu]: true }));
+  };
+
+  const handleMouseLeave = (menu: string) => {
+    setDropdown(prevState => ({ ...prevState, [menu]: false }));
+  };
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" className="navbar-logo">
+          <img src={Logo} alt="Logo" className="navbar-logo-img" />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Sevak Digital Technologies
+        </a>
+        <ul className="nav-menu">
+          <li
+            className="nav-item"
+            onMouseEnter={() => handleMouseEnter('home')}
+            onMouseLeave={() => handleMouseLeave('home')}
+          >
+            <a href="#home" className="nav-links">Home</a>
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={() => handleMouseEnter('products')}
+            onMouseLeave={() => handleMouseLeave('products')}
+          >
+            <a href="#products" className="nav-links">
+              Products <i className="fas fa-caret-down"></i>
+            </a>
+            {dropdown.products && (
+              <ul className="dropdown-menu">
+                <li className="dropdown-item">
+                  <a
+                    href="#products"
+                    className="dropdown-links"
+                    onClick={() => handleNavigation('/product')}
+                  >
+                    SMS
+                  </a>
+                </li>
+                <li className="dropdown-item">
+                  <a
+                    href="#products2"
+                    className="dropdown-links"
+                    onClick={() => handleNavigation('/product')}
+                  >
+                    S2 SERVICES
+                  </a>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={() => handleMouseEnter('solutions')}
+            onMouseLeave={() => handleMouseLeave('solutions')}
+          >
+            <a href="#solutions" className="nav-links">
+              Solutions <i className="fas fa-caret-down"></i>
+            </a>
+            {dropdown.solutions && (
+              <ul className="dropdown-menu">
+                <li className="dropdown-item">
+                  <a href="#solution1" className="dropdown-links">Solution 1</a>
+                </li>
+                <li className="dropdown-item">
+                  <a href="#solution2" className="dropdown-links">Solution 2</a>
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
